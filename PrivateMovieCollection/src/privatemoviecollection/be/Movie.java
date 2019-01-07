5423539f9e63bcab8b07e28b981315ca8bbc1f9b
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-07 12:15:02 +0100

    Create Movie.java
    Creating Movie business entity

---
+++ PrivateMovieCollection/src/privatemoviecollection/be/Movie.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.be;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Acer
+ */
+public class Movie {
+    
+    private String title;
+    private List<String> categories;
+    private double rating;
+    private String path;
+    private int time;
+    
+    public Movie(String title, String path, int time)
+    {
+        this.title = title;
+        this.path = path;
+        this.time = time;
+        categories = new ArrayList();
+    }
+
+    public String getTitle() 
+    {
+        return title;
+    }
+
+    public void setTitle(String title) 
+    {
+        this.title = title;
+    }
+    
+    public void addCategory(String category)
+    {
+        categories.add(category);
+    }
+    
+    public void removeCategory(String category)
+    {
+        categories.remove(category);
+    }
+
+    public double getRating() 
+    {
+        return rating;
+    }
+
+    public void setRating(double rating) 
+    {
+        this.rating = rating;
+    }
+
+    public String getPath() 
+    {
+        return path;
+    }
+
+    public void setPath(String path) 
+    {
+        this.path = path;
+    }
+
+    public int getTime() 
+    {
+        return time;
+    }
+    
+    
+    
+}
