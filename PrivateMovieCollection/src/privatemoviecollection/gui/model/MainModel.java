a7f53214359c482f36867e2bccaa7611d25d3fa2
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-08 14:05:32 +0100

    Update MainModel.java
    Using singleton pattern on MainModel + creating a connection with BLL + implementing createMovie method

--- PrivateMovieCollection/src/privatemoviecollection/gui/model/MainModel.java
+++ PrivateMovieCollection/src/privatemoviecollection/gui/model/MainModel.java
@@ -9,7 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import privatemoviecollection.be.Category;
 import privatemoviecollection.be.Movie;
+import privatemoviecollection.bll.BllManager;
+import privatemoviecollection.bll.IBllFacade;
 
 /**
  *
@@ -17,16 +20,28 @@ import privatemoviecollection.be.Movie;
  */
 public class MainModel {
     
+    private static MainModel instance;
+    private IBllFacade bllManager;
     private ObservableList<Movie> movies;
     
-    public MainModel()
+    private MainModel()
     {
+        bllManager = new BllManager();
         movies = FXCollections.observableArrayList();
         
         //temporary - later we will fetch data from database
-        List<String> categories = new ArrayList();
-        categories.add("Biography");
-        movies.add(new Movie("Ray", categories, "path to file", 999999));
+        List<Category> categories = new ArrayList();
+        categories.add(new Category(1, "Biography"));
+        movies.add(new Movie(1, "Ray", categories, "path to file", 999999));
+    }
+    
+    public static MainModel createInstance()
+    {
+        if(instance == null)
+        {
+            instance = new MainModel();
+        }
+        return instance;
     }
     
     public ObservableList<Movie> getMovies()
@@ -34,8 +49,14 @@ public class MainModel {
         return movies;
     }
 
-    public void deleteMovie(Movie selectedMovie) {
-        movies.remove(selectedMovie);
+    public void createMovie(String title, List<Category> categories, String path, int time, Double rating)
+    {
+        Movie createdMovie = bllManager.createMovie(title, categories, path, time, rating);
+        movies.add(createdMovie);
+    }
+    
+    public void deleteMovie(Movie movie) {
+        movies.remove(movie);
     }
     
     
