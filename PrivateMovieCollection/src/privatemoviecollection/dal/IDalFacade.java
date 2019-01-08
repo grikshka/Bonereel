0f5e850a4d692a4a8dff38cd9bd2947245eacc44
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-08 14:02:32 +0100

    Create IDalFacade.java
    Create IDalFacade + adding createMovie method

---
+++ PrivateMovieCollection/src/privatemoviecollection/dal/IDalFacade.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.dal;
+
+import java.sql.SQLException;
+import java.util.List;
+import privatemoviecollection.be.Category;
+import privatemoviecollection.be.Movie;
+
+/**
+ *
+ * @author Acer
+ */
+public interface IDalFacade {
+    
+    Movie createMovie(String title, List<Category> categories, String path, int time, Double rating);
+    
+}
