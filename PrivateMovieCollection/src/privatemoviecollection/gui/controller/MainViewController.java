2d59b91ba9f7ff5c608f914ebdcc37db272b9be9
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-08 11:21:21 +0100

    Update MainViewController
    Fixing clickEditMovies method + disabling buttons for editing and removing movies + adding code for displaying items in tableview

--- PrivateMovieCollection/src/privatemoviecollection/gui/controller/MainViewController.java
+++ PrivateMovieCollection/src/privatemoviecollection/gui/controller/MainViewController.java
@@ -7,6 +7,7 @@ package privatemoviecollection.gui.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,24 +15,64 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import privatemoviecollection.be.Movie;
+import privatemoviecollection.gui.model.MainModel;
 
 /**
  *
  * @author Acer
  */
 public class MainViewController implements Initializable {
+    
+    private MainModel model;
 
     @FXML
     private TableView<Movie> tblMovies;
+    @FXML
+    private TableColumn<Movie, String> colTitle;
+    @FXML
+    private TableColumn<Movie, String> colCategories;
+    @FXML
+    private TableColumn<Movie, Integer> colTime;
+    @FXML
+    private TableColumn<Movie, String> colRating;
+    @FXML
+    private Button btnRemoveMovie;
+    @FXML
+    private Button btnEditMovie;
+    
+    public MainViewController()
+    {
+        model = new MainModel();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        disableElements();
+        loadData();
     }    
+    
+    private void disableElements()
+    {
+        btnEditMovie.setDisable(true);
+        btnRemoveMovie.setDisable(true);
+    }
+    
+    private void loadData()
+    {
+        colTitle.setCellValueFactory(new PropertyValueFactory("title"));
+        colCategories.setCellValueFactory(new PropertyValueFactory("categoriesInString"));
+        colTime.setCellValueFactory(new PropertyValueFactory("time"));
+        colRating.setCellValueFactory(new PropertyValueFactory("rating"));
+        tblMovies.setItems(model.getMovies());
+    }
 
     @FXML
     private void clickEditCategories(ActionEvent event) throws IOException {
@@ -59,14 +100,23 @@ public class MainViewController implements Initializable {
     private void clickEditMovie(ActionEvent event) throws IOException {
         Movie selectedMovie = tblMovies.getSelectionModel().getSelectedItem();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/privatemoviecollection/gui/view/MovieView.fxml"));
-        MovieViewController controller = fxmlLoader.getController();
-        controller.setEditingMode(selectedMovie);
         Parent root = (Parent) fxmlLoader.load();
+        MovieViewController controller = (MovieViewController) fxmlLoader.getController();
+        controller.setEditingMode(selectedMovie);
         Stage stage = new Stage();
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setTitle("Edit Movie");
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    @FXML
+    private void clickOnMovies(MouseEvent event) {
+        if(tblMovies.getSelectionModel().getSelectedItem() != null)
+        {
+            btnEditMovie.setDisable(false);
+            btnRemoveMovie.setDisable(false);
+        }
     }
     
 }
