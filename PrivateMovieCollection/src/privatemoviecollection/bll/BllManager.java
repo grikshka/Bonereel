868bdc204b52426ea32d12d2e6d8790cd7058410
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-08 14:04:00 +0100

    Create BllManager.java
    Create BllManager which is implementing IBllFacade + implementing createMovie method

---
+++ PrivateMovieCollection/src/privatemoviecollection/bll/BllManager.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.bll;
+
+import java.util.List;
+import privatemoviecollection.be.Category;
+import privatemoviecollection.be.Movie;
+import privatemoviecollection.dal.DalController;
+import privatemoviecollection.dal.IDalFacade;
+
+/**
+ *
+ * @author Acer
+ */
+public class BllManager implements IBllFacade{
+    
+    private IDalFacade dalController;
+    
+    public BllManager()
+    {
+        dalController = new DalController();
+    }
+
+    @Override
+    public Movie createMovie(String title, List<Category> categories, String path, int time, Double rating) 
+    {
+        return dalController.createMovie(title, categories, path, time, rating);
+    }
+    
+}
