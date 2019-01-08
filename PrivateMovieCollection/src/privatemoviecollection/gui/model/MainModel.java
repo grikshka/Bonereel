e7fb6c79edcfef64742165b38a2277e7b1aec8d8
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-08 11:19:34 +0100

    Create MainModel.java
    Creating MainModel with temporarily hardcoded data.

---
+++ PrivateMovieCollection/src/privatemoviecollection/gui/model/MainModel.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.gui.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import privatemoviecollection.be.Movie;
+
+/**
+ *
+ * @author Acer
+ */
+public class MainModel {
+    
+    private ObservableList<Movie> movies;
+    
+    public MainModel()
+    {
+        movies = FXCollections.observableArrayList();
+        
+        //temporary - later we will fetch data from database
+        List<String> categories = new ArrayList();
+        categories.add("Biography");
+        movies.add(new Movie("Ray", categories, "path to file", 999999));
+    }
+    
+    public ObservableList<Movie> getMovies()
+    {
+        return movies;
+    }
+    
+    
+}
