bb8f95f4529ea16d29b7193bcd7f8ab6fb7ab441
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-09 09:49:51 +0100

    Create CategoriesModel.java
    Create CategoriesModel using singleton pattern + implementing createCategory and getCategories methods + creating connection with BLL

---
+++ PrivateMovieCollection/src/privatemoviecollection/gui/model/CategoriesModel.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.gui.model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import privatemoviecollection.be.Category;
+import privatemoviecollection.bll.BllManager;
+import privatemoviecollection.bll.IBllFacade;
+
+/**
+ *
+ * @author Acer
+ */
+public class CategoriesModel {
+    
+    private static CategoriesModel instance;
+    private IBllFacade bllManager;
+    private ObservableList<Category> categories;
+    
+    private CategoriesModel()
+    {
+        bllManager = new BllManager();
+        categories = FXCollections.observableArrayList(bllManager.getAllCategories());
+    }
+    
+    public static CategoriesModel createInstance()
+    {
+        if(instance == null)
+        {
+            instance = new CategoriesModel();
+        }
+        return instance;
+    }
+    
+    public ObservableList<Category> getCategories()
+    {
+        return categories;
+    }
+    
+    public void createCategory(String name)
+    {
+        Category createdCategory = bllManager.createCategory(name);
+        categories.add(createdCategory);
+    }
+}
