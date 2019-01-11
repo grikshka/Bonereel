c315e36233ee8ddbddd5c5e35a2e1ddc474ff9e9
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-11 11:36:46 +0100

    Update MovieDAO.java
    Fixing createMovie method - right now it is working if user dont specify the rating of movie + implementing getAllMovies method

--- PrivateMovieCollection/src/privatemoviecollection/dal/daos/MovieDAO.java
+++ PrivateMovieCollection/src/privatemoviecollection/dal/daos/MovieDAO.java
@@ -10,6 +10,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
+import java.util.ArrayList;
 import java.util.List;
 import privatemoviecollection.be.Category;
 import privatemoviecollection.be.Movie;
@@ -42,7 +44,11 @@ public class MovieDAO {
             statement.setString(1, title);
             statement.setString(2, path);
             statement.setInt(3, time);
-            if(rating != null)
+            if(rating == null)
+            {
+                statement.setNull(4, Types.DOUBLE);
+            }
+            else
             {
                 statement.setDouble(4, rating);
             }
@@ -53,6 +59,33 @@ public class MovieDAO {
             Movie movie = new Movie(id, title, categories, path, time, rating);
             mcDao.addCategoriesToMovie(movie, categories);
             return movie;
+        }
+    }
+    
+    public List<Movie> getAllMovies() throws SQLException
+    {
+        String sqlStatement = "SELECT * FROM Movies";
+        List<Movie> allMovies = new ArrayList();
+        try(Connection con = connector.getConnection();
+                PreparedStatement statement = con.prepareStatement(sqlStatement))
+        {
+            ResultSet rs = statement.executeQuery();
+            while(rs.next())
+            {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                double primitiveRating = rs.getDouble("rating");
+                Double rating = null;
+                if(primitiveRating != 0)
+                {
+                    rating = primitiveRating;
+                }
+                String path = rs.getString("path");
+                int time = rs.getInt("time");
+                allMovies.add(new Movie(id, title, path, time, rating));
+            }
+            mcDao.addCategoriesToAllMovies(allMovies);
+            return allMovies;
         }
     }
     
