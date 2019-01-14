e6cb08daba93f2af50326746dffc392661313033
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-14 15:05:37 +0100

    Update MoviePlayerViewController.java
    Adding methods for hiding/showing the hud + adding method for resuming/pausing video while clicking on a mediaView + adding method for closing window

--- PrivateMovieCollection/src/privatemoviecollection/gui/controller/MoviePlayerViewController.java
+++ PrivateMovieCollection/src/privatemoviecollection/gui/controller/MoviePlayerViewController.java
@@ -7,20 +7,25 @@ package privatemoviecollection.gui.controller;
 
 import java.io.File;
 import java.net.URL;
+import java.util.EventObject;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import privatemoviecollection.be.Movie;
 import privatemoviecollection.bll.util.TimeConverter;
@@ -49,6 +54,19 @@ public class MoviePlayerViewController implements Initializable {
     private Label lblMovieCurrentTime;
     @FXML
     private Label lblMovieEndTime;
+    @FXML
+    private ToggleButton btnPlay;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private Label lblTitle;
+    @FXML
+    private Label lblSlash;
+    @FXML
+    private Rectangle rctBottom;
+    @FXML
+    private Rectangle rctTop;
+
 
     /**
      * Initializes the controller class.
@@ -105,6 +123,7 @@ public class MoviePlayerViewController implements Initializable {
         mediaPlayer = new MediaPlayer(mediaFile);
         setTimeListener();
         sldTime.setMax(movie.getTime());
+        lblTitle.setText(movie.getTitle());
         lblMovieEndTime.setText(movie.getTimeInString());
         mdvPlayer.setMediaPlayer(mediaPlayer);
         mediaPlayer.setVolume(sldVolume.getValue());
@@ -164,5 +183,80 @@ public class MoviePlayerViewController implements Initializable {
         mediaPlayer.seek(Duration.seconds(sldTime.getValue()));
         movieTimeChanged=true;
     }
+
+    @FXML
+    private void clickClose(ActionEvent event) {
+        mediaPlayer.stop();
+        Stage stage = (Stage)((Node)((EventObject) event).getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void hoverOnHud(MouseEvent event) {
+        sldTime.setVisible(true);
+        sldVolume.setVisible(true);
+        btnMute.setVisible(true);
+        btnPlay.setVisible(true);
+        lblMovieCurrentTime.setVisible(true);
+        lblMovieEndTime.setVisible(true);
+        lblTitle.setVisible(true);
+        lblSlash.setVisible(true);
+        rctTop.setVisible(true);
+        rctBottom.setVisible(true);
+
+    }
+
+    @FXML
+    private void hoverOffHud(MouseEvent event) {
+        if(!mediaPlayer.getStatus().equals(MediaPlayer.Status.PAUSED))
+        {
+            sldTime.setVisible(false);
+            sldVolume.setVisible(false);
+            btnMute.setVisible(false);
+            btnPlay.setVisible(false);
+            lblMovieCurrentTime.setVisible(false);
+            lblMovieEndTime.setVisible(false);
+            lblTitle.setVisible(false);
+            lblSlash.setVisible(false);
+            rctTop.setVisible(false);
+            rctBottom.setVisible(false);
+        }
+    }
+
+    @FXML
+    private void clickOnMediaView(MouseEvent event) 
+    {
+        if(mediaPlayer.getStatus().equals(MediaPlayer.Status.PLAYING))
+        {
+            mediaPlayer.pause();
+            //change play button image
+            sldTime.setVisible(true);
+            sldVolume.setVisible(true);
+            btnMute.setVisible(true);
+            btnPlay.setVisible(true);
+            lblMovieCurrentTime.setVisible(true);
+            lblMovieEndTime.setVisible(true);
+            lblTitle.setVisible(true);
+            lblSlash.setVisible(true);
+            rctTop.setVisible(true);
+            rctBottom.setVisible(true);
+        }
+        else
+        {
+            mediaPlayer.play();
+            //change play button image
+            sldTime.setVisible(false);
+            sldVolume.setVisible(false);
+            btnMute.setVisible(false);
+            btnPlay.setVisible(false);
+            lblMovieCurrentTime.setVisible(false);
+            lblMovieEndTime.setVisible(false);
+            lblTitle.setVisible(false);
+            lblSlash.setVisible(false);
+            rctTop.setVisible(false);
+            rctBottom.setVisible(false);
+        }
+    }
+
     
 }
