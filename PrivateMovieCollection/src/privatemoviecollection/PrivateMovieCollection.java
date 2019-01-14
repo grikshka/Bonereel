0f923c63bc8b0931a84152d545fa8b90adbea430
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-01-14 10:20:26 +0100

    Update PrivateMovieCollection.java
    Changing starting view to LoginView

--- PrivateMovieCollection/src/privatemoviecollection/PrivateMovieCollection.java
+++ PrivateMovieCollection/src/privatemoviecollection/PrivateMovieCollection.java
@@ -19,7 +19,7 @@ public class PrivateMovieCollection extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gui/view/MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/LoginView.fxml"));
         
         Scene scene = new Scene(root);
         
